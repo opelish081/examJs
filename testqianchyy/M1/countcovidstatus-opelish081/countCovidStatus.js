e149059394b0d78e5f29b7fc38b8d69ed69df58f
qian <88130042+opelish081@users.noreply.github.com>
2022-12-26 08:58:18 +0700

    Add files via upload

--- testqianchyy/M1/countcovidstatus-opelish081/countCovidStatus.js
+++ testqianchyy/M1/countcovidstatus-opelish081/countCovidStatus.js
@@ -9,33 +9,33 @@ else if(covidStatusArray.length==0){
   return {}
 }
 
-else {
-  let negativeandlowrisk = 0
-  let negativeandhighrisk = 0
+
+  let lowrisk = 0
+  let highrisk = 0
   let positive = 0
   let result = {}
 for (const covidStatus of covidStatusArray){
   if(covidStatus === 'negative and low risk'){
-    negativeandlowrisk++
+    lowrisk++
   }
   else if(covidStatus === 'negative and high risk'){
-    negativeandhighrisk++
+    highrisk++
   }
   else if(covidStatus === 'positive'){
     positive++
   }
 }
-  if(negativeandlowrisk > 0) {
-    result['negative and low risk'] = negativeandlowrisk
+  if(lowrisk > 0) {
+    result['negative and low risk'] = lowrisk
   }
-  if (negativeandhighrisk > 0) {
-    result['negative and high risk'] = negativeandhighrisk
+  if (highrisk > 0) {
+    result['negative and high risk'] = highrisk
   }
   if (positive > 0) {
     result['positive'] = positive
   }
   return result
 }
-}
+
 
 module.exports = countCovidStatus
--- testqianchyy/M1/test-countposnegvalue-opelish081-main/countPosNegValue.js
+++ testqianchyy/M1/test-countposnegvalue-opelish081-main/countPosNegValue.js
@@ -1,38 +1,24 @@
 const { template } = require('@babel/core')
 //64130500112 สรวิชญ์ จงจามรีสีทอง
-// function countPosNegValue(posNegIntegerArray) {
-//   if (posNegIntegerArray === null || posNegIntegerArray === undefined) {
-//     return undefined;
-//   }
-//   if (posNegIntegerArray.length === 0) {
-//     return {};
-//   }
-//   let count = {
-//     positive: 0,
-//     negative: 0
-//   };
-
-//   for (let i = 0; i < posNegIntegerArray.length; i++) {
-//     if (posNegIntegerArray[i] > 0) {
-//       count.positive++;
-//     } else if (posNegIntegerArray[i] < 0) {
-//       count.negative++;
-//     }
-//   }
-//   return count;
-// }
-
 function countPosNegValue(posNegIntegerArray) {
-  if (!posNegIntegerArray) return undefined;
-  if (posNegIntegerArray.length === 0) return {};
+  if(posNegIntegerArray === null || posNegIntegerArray === undefined){
+    return undefined
+  }
+  else if(posNegIntegerArray.length === 0){
+    return {}
+  }
+  let pos = 0
+  let neg = 0
 
-  return posNegIntegerArray.reduce((acc, val) => {
-    if (Math.sign(val) === 1) acc.positive++;
-    if (Math.sign(val) === -1) acc.negative++;
-    return acc;
-  }, { positive: 0, negative: 0 });
-}
-
+  for(let num of posNegIntegerArray){
+    if(num > 0){
+      pos++
+    }else if(num < 0){
+      neg++
+    }
+  }
+  return { positive: pos, negative: neg} 
+  }
 
 module.exports = countPosNegValue
 
